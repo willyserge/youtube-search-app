4fe5b021cba99774be7af014c2949b6631a1aa96
willyserge <willyserge01@gmail.com>
2021-07-05 11:44:59 +0200

    create search input

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
+import { Pane, SearchInput } from 'evergreen-ui';
+
 function App() {
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <div className="App">
-      youtube search app
+      <Pane padding={30}>
+        <SearchInput
+          width="100%"
+          height={40}
+          placeholder="Search for videos..."
+          value={searchValue}
+          onChange={handleChange}
+        />
+      </Pane>
     </div>
   );
 }
